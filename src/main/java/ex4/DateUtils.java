a54f453cd50afca3e35218a6cd85598f751bdc08
Alegria <pecalcei@gmail.com>
2023-07-10 15:29:36 +0200

    exercice 4 - terminéé

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -6,14 +6,10 @@ import java.util.Date;
 public class DateUtils {
 
 	public static String format(String pattern, Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
-		return formateur.format(date);
+		return new SimpleDateFormat(pattern).format(date);
 	}
-	
+
 	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		return formateur.format(date);
+		return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(date);
 	}
 }
